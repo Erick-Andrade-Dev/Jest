115eebbe3e13cb3d0a32421bf62a49b3ee15b963
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-08-01 15:52:06 -0300

    jest on object

---
+++ src/info.js
@@ -0,0 +1,8 @@
+const info = {
+    name: {first: 'Erick', last:'Andrade'},
+    age: 21,
+    profession: 'developer',
+    birth:{ day: 1, mouth:'august', age:2001}    
+}
+
+module.exports = info
